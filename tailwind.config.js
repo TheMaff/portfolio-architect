23a02ff8172cfcae6b1ee519350fc93c5902df3b
Marcos Flores <maff.dsgn@gmail.com>
2026-03-28 00:52:21 -0300

    chore: setup tailwind theme and global directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'brand-dark': '#0f172a', // Un azul oscuro muy "Premium/Tech"
-        'brand-accent': '#3b82f6', 
+        'brand-accent': '#3b82f6',
       },
     },
   },
